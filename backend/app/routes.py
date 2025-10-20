c9012820b0f5d81c2115a9a5ed2a91a16c3a28b5
aleedoe <mushyafaali@gmail.com>
2025-10-20 15:19:48 +0700

    add api for get all employees

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,3 +2,9 @@ from flask import Blueprint
 
 admin_bp = Blueprint('admin', __name__)
 employee_bp = Blueprint('employee', __name__)
+
+from .controllers.employee import get_all_employees
+
+admin_bp.route('/employees', methods=['GET'])(get_all_employees)
+
+
