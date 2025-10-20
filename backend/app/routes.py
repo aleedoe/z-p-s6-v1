ef50045faa95c6d4502c4044b8ff8857af9ebed4
aleedoe <mushyafaali@gmail.com>
2025-10-20 16:10:12 +0700

    add route ateendance

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .controllers.admin import get_all_employees, get_employee_by_id, get_all_work_schedules, get_work_schedule_by_id
+from .controllers.admin import get_all_employees, get_employee_by_id, get_all_work_schedules, get_work_schedule_by_id, get_all_attendance, get_attendance_by_id
 
 admin_bp = Blueprint('admin', __name__)
 employee_bp = Blueprint('employee', __name__)
@@ -9,4 +9,6 @@ admin_bp.route('/employees', methods=['GET'])(get_all_employees)
 admin_bp.route('/employees/<int:id_employee>', methods=['GET'])(get_employee_by_id)
 admin_bp.route('/work-schedules', methods=['GET'])(get_all_work_schedules)
 admin_bp.route('/work-schedules/<int:id_schedule>', methods=['GET'])(get_work_schedule_by_id)
+admin_bp.route('/attendance', methods=['GET'])(get_all_attendance)
+admin_bp.route('/attendance/<int:attendance_id>', methods=['GET'])(get_attendance_by_id)
 
