0aff0fb6ff0fdaf72f9a884ba4c62a5cdfa466db
aleedoe <mushyafaali@gmail.com>
2025-10-23 07:50:59 +0700

    update employe service

--- frontend/web/services/employee.service.ts
+++ frontend/web/services/employee.service.ts
@@ -1,4 +1,4 @@
-import { Employee, EmployeesResponse, PaginationParams } from "@/types/api/employee";
+import { Employee, EmployeesResponse, PaginationParams, EmployeeDetailResponse } from "@/types/api/employee";
 import { BaseService } from "./base.service";
 
 class EmployeeService extends BaseService {
@@ -8,25 +8,35 @@ class EmployeeService extends BaseService {
         return this.get<EmployeesResponse>(this.endpoint, { params });
     }
 
-    async getById(id: number): Promise<Employee> {
-        return this.get<Employee>(`${this.endpoint}/${id}`);
+    async getById(id: number): Promise<EmployeeDetailResponse> {
+        return this.get<EmployeeDetailResponse>(`${this.endpoint}/${id}`);
     }
 
-    async create(data: Omit<Employee, 'id'>): Promise<Employee> {
-        return this.post<Employee>(this.endpoint, data);
+    async create(data: {
+        nik: string;
+        name: string;
+        gender: string;
+        position: string;
+        email: string;
+        password: string;
+    }): Promise<{ message: string; id: number }> {
+        return this.post<{ message: string; id: number }>(this.endpoint, data);
     }
 
-    async update(id: number, data: Partial<Employee>): Promise<Employee> {
-        return this.put<Employee>(`${this.endpoint}/${id}`, data);
+    async update(id: number, data: {
+        nik?: string;
+        name?: string;
+        gender?: string;
+        position?: string;
+        email?: string;
+        password?: string;
+    }): Promise<{ message: string }> {
+        return this.put<{ message: string }>(this.endpoint + `/${id}`, data);
     }
 
-    async partialUpdate(id: number, data: Partial<Employee>): Promise<Employee> {
-        return this.patch<Employee>(`${this.endpoint}/${id}`, data);
+    async delete(id: number): Promise<{ message: string }> {
+        return this.delete<{ message: string }>(`${this.endpoint}/${id}`);
     }
-
-    // async delete(id: number): Promise<void> {
-    //     return this.delete<void>(`${this.endpoint}/${id}`);
-    // }
 
     async search(query: string): Promise<EmployeesResponse> {
         return this.get<EmployeesResponse>(`${this.endpoint}/search`, {
